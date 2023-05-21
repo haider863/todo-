382758d5250727e2d5c3e5625197b4e73f3b8f4d
haider863 <haidermanzar863@gmail.com>
2023-05-22 01:28:52 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-  import logo from './logo.svg';
-import './App.css';
-import MyTodo from './Todo1';
+import logo from "./logo.svg";
+import "./App.css";
+import MyTodo from "./Todo1";
 function App() {
   return (
     <div className="App">
-           <p>haider</p>
-           <MyTodo />
+      <p>haider todo app</p>
+      <MyTodo />
     </div>
   );
 }
